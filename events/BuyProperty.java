068118a0e87d9b37c3be81d32c91b6d597d7230f
Brittany <bfram2@uis.edu>
2016-10-02 22:56:39 -0500

    Create BuyProperty.java

---
+++ events/BuyProperty.java
@@ -0,0 +1,38 @@
+//Flat out buying any property & Mortaging it for half of the cost of buying the property
+
+/*
+Purple (#44235A)
+•	Mediterranean Avenue = Germania Inferior ($60, $30 mortgaged)
+•	Baltic Avenue = Germania Superior ($60, $30 mortgaged)
+Baby Blue (#C4C8E5)
+•	Oriental Avenue = Alpes Poeniae ($100, $50 mortgaged)
+•	Vermont Avenue = Alpes Cottiae ($100, $50 mortgaged)
+•	Connecticut Avenue = Aples Maritimae ($120, $60 mortgaged)
+Dark Pink (#A72758)
+•	St. Charles Place = Aquitania ($140, $70 mortgaged)
+•	States Avenue = Belgica ($140, $70 mortgaged)
+•	Virginia Avenue = Raetia ($160, $80 mortgaged)
+Orange (#F19749)
+•	St. James Place = Africa Proconsularis ($180, $90 mortgaged)
+•	Tennessee Avenue = Asia ($180, $90 mortgaged)
+•	New York Avenue = Britannia ($200, $100 mortgaged)
+Red (#E22835)
+•	Kentucky Avenue = Cilicia ($220, $110 mortgaged)
+•	Indiana Avenue = Galatia ($220, $110 mortgaged)
+•	Illinois Avenue = Cappadocia ($240, $120 mortgaged)
+Yellow (#F8F37B)
+•	Atlantic Avenue = Aegyptus ($260, $130 mortgaged)
+•	Ventnor Avenue = Arabia Petraea ($260, $130 mortgaged)
+•	Marvin Gardens = Syria ($280, $140 mortgaged)
+Green (#448364)
+•	Pacific Avenue = Macedonia ($300, $150 mortgaged)
+•	North Carolina Avenue = Epirus ($300, $150 mortgaged)
+•	Pennsylvania Avenue = Achaia ($320, $160 mortgaged)
+Blue (#413382)
+•	Park Place = Sicilia ($350, $175 mortgaged)
+•	Boardwalk = Italia ($400, $200 mortgaged)
+Utilities
+$150 buy, $75 mortgaged
+Roads
+$200 buy, $100 mortgaged
+*/
