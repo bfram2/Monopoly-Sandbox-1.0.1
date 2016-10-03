f730639e214ee81f0c77ba53cbbf4b9be7f03321
Brittany <bfram2@uis.edu>
2016-10-02 22:41:40 -0500

    Create Money.java

---
+++ pieces/Money.java
@@ -0,0 +1,13 @@
+//Money using Roman conversion
+//Use images of the dollar bills or money counter with just the digits ie. 100.00 denarius
+
+/*
+20 $500 Bills (orange) = aureus
+20 $100 Bills (beige) = denarius
+30 $50 Bills (blue) = sestertius
+50 $20 Bills (green) = dupondius
+40 $10 Bills (yellow) = as
+40 $5 Bills (pink) = semis
+40 $1 Bills (white) = triens
+
+*/
