a5a8ccee51787d4dfcf5efd91a733c3b6df5d4c3
Brittany <bfram2@uis.edu>
2016-10-02 23:19:47 -0500

    Update Arena.java

--- events/Arena.java
+++ events/Arena.java
@@ -1,3 +1,8 @@
-//Go to Jail, or Go to the Gladitorial Arena
-//Check for Get out of the Arena free card.
-//2 set of coding (later): If doubles, roll again, if doubles a second time go to jail
+//Go to Jail: Go to the Gladitorial Arena
+
+/*
+Check for Get out of the Arena free card, then move to the arena
+wait for player to roll doubles to get out or to receive a get out of the arena free card
+
+2nd set of coding (later): If doubles, roll again, if doubles a second time go to jail
+*/
