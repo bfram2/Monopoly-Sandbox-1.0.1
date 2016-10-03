a2417e89266c1d7d897324d59e5b87ab8ff5a27f
Brittany <bfram2@uis.edu>
2016-10-02 22:50:43 -0500

    Update and rename TokensVillasPantheons.java to VillasPantheons.java

--- pieces/TokensVillasPantheons.java
+++
@@ -1,6 +0,0 @@
-// 8 Tokens, 32 Villas, 12 Pantheons
-
-/*
-Tokens: soldier, senator in toga, women in stola, merchant & cart, chariot, sandal, coin, horse
-
-*/
---
+++ pieces/VillasPantheons.java
@@ -0,0 +1,2 @@
+// 32 Villas, 12 Pantheons, using 3D like images with transparency
+
