d8a8693f38c5927d499caaa46949faa988b60ce5
Brittany <bfram2@uis.edu>
2016-10-02 23:23:28 -0500

    Create HelpMenu.java

---
+++ HelpMenu.java
@@ -0,0 +1,6 @@
+//User Friendly summaried version of the User Manual
+
+/*
+depending on what move the player is on is which paragraph will appear as the main page,
+can still sift through clicking on titles and expanding to the descriptions
+*/
