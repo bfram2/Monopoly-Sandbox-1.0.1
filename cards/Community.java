31a3c58b6d2e40c11810859ab4632cf4d948b286
Brittany <bfram2@uis.edu>
2016-10-02 22:27:33 -0500

    Update Community.java

--- cards/Community.java
+++ cards/Community.java
@@ -1,1 +1,20 @@
 /* For Community Cards */
+
+/*
+Bank error in your favor. Collect $200.
+Pay private physician fees of $100.
+From sale of merchant carts, you get $50.
+You inherit a small Villa, gain $100.
+Go to the Gladiatorial Arena. Go directly to the arena, DO NOT pass Rome, DO NOT collect $200.
+Advance to Rome. Collect $200.
+Render too much unto Caesar, Collect $200.
+Roman official assesses you for street repairs: $40 per villa, $115 per hotel.
+You have won the second price in the Campus Track & Field foot race. Collect $10.
+Your properties and assets mature, join the Equestrian Order. Collect $100. [19]
+Sell bulk staples to the bank, receive $100.
+Pay farmer’s tax of $25.
+You have a coming of age party, Collect $10 from every player.
+Pay school fees of $50.
+Medical institution fees, pay $50.
+Gain a favor with a Senator, get out of the Gladiatorial Area for free. This card may be kept until needed or traded.
+*/
