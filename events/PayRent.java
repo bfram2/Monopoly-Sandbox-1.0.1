84ddd41a86ea3d2a35ac0a7914e9fbc54f836063
Brittany <bfram2@uis.edu>
2016-10-02 23:00:37 -0500

    Create PayRent.java

---
+++ events/PayRent.java
@@ -0,0 +1,29 @@
+//Paying Rent on any Property
+
+/*
+Germania Inferior: Rent $2, With 1 Villa $10, With 2 Villas $30, With 3 Villas $90 With 4 Villas $160, with Pantheon $250, Houses cost $50 each, Hotels: $50 each plus 4 houses
+Germania Superior: Rent $4, With 1 Villa $20, With 2 Villas $60, With 3 Villas $180 With 4 Villas $320, with Pantheon $450, Houses cost $50 each, Hotels: $50 each plus 4 houses
+Alpes Poeniae: Rent $6, With 1 Villa $30, With 2 Villas $90, With 3 Villas $270 With 4 Villas $400, with Pantheon $550, Houses cost $50 each, Hotels: $50 each plus 4 houses
+Alpes Cottiae: Rent $6, With 1 Villa $30, With 2 Villas $90, With 3 Villas $270 With 4 Villas $400, with Pantheon $550, Houses cost $50 each, Hotels: $50 each plus 4 houses
+Aples Maritimae: Rent $8, With 1 Villa $40, With 2 Villas $100, With 3 Villas $300 With 4 Villas $450, with Pantheon $600, Houses cost $50 each, Hotels: $50 each plus 4 houses
+Aquitania: Rent $10, With 1 Villa $50, With 2 Villas $150, With 3 Villas $450 With 4 Villas $625, with Pantheon $750, Houses cost $100 each, Hotels: $100 each plus 4 houses
+Belgica: Rent $10, With 1 Villa $50, With 2 Villas $150, With 3 Villas $450 With 4 Villas $625, with Pantheon $750, Houses cost $100 each, Hotels: $100 each plus 4 houses
+Raetia: Rent $12, With 1 Villa $60, With 2 Villas $180, With 3 Villas $500 With 4 Villas $700, with Pantheon $900, Houses cost $100 each, Hotels: $100 each plus 4 houses
+Africa Proconsularis: Rent $14, With 1 Villa $70, With 2 Villas $200, With 3 Villas $550 With 4 Villas $750, with Pantheon $950, Houses cost $100 each, Hotels: $100 each plus 4 houses
+Asia: Rent $14, With 1 Villa $70, With 2 Villas $200, With 3 Villas $550 With 4 Villas $750, with Pantheon $950, Houses cost $100 each, Hotels: $100 each plus 4 houses
+Britannia: Rent $16, With 1 Villa $80, With 2 Villas $220, With 3 Villas $600 With 4 Villas $800, with Pantheon $1000, Houses cost $100 each, Hotels: $100 each plus 4 houses
+Cilicia: Rent $18, With 1 Villa $90, With 2 Villas $250, With 3 Villas $700 With 4 Villas $875, with Pantheon $1050, Houses cost $150 each, Hotels: $150 each plus 4 houses
+Galatia: Rent $18, With 1 Villa $90, With 2 Villas $250, With 3 Villas $700 With 4 Villas $875, with Pantheon $1050, Houses cost $150 each, Hotels: $150 each plus 4 houses
+Cappadocia: Rent $20, With 1 Villa $100, With 2 Villas $300, With 3 Villas $750 With 4 Villas $925, with Pantheon $1100, Houses cost $150 each, Hotels: $150 each plus 4 houses
+Aegyptus: Rent $22, With 1 Villa $110, With 2 Villas $330, With 3 Villas $800 With 4 Villas $975, with Pantheon $1150, Houses cost $150 each, Hotels: $150 each plus 4 houses 
+Arabia Petraea: Rent $22, With 1 Villa $110, With 2 Villas $330, With 3 Villas $800 With 4 Villas $975, with Pantheon $1150, Houses cost $150 each, Hotels: $150 each plus 4 houses
+Syria: Rent $24, With 1 Villa $120, With 2 Villas $360, With 3 Villas $850 With 4 Villas $1025, with Pantheon $1200, Houses cost $150 each, Hotels: $150 each plus 4 houses
+Macedonia: Rent $26, With 1 Villa $130, With 2 Villas $390, With 3 Villas $900 With 4 Villas $1100, with Pantheon $1275, Houses cost $200 each, Hotels: $200 each plus 4 houses
+Epirus: Rent $26, With 1 Villa $130, With 2 Villas $390, With 3 Villas $900 With 4 Villas $1100, with Pantheon $1275, Houses cost $200 each, Hotels: $200 each plus 4 houses
+Achaia: Rent $28, With 1 Villa $150, With 2 Villas $450, With 3 Villas $1000 With 4 Villas $1200, with Pantheon $1400, Houses cost $200 each, Hotels: $200 each plus 4 houses
+Sicilia: Rent $35, With 1 Villa $175, With 2 Villas $500, With 3 Villas $1100 With 4 Villas $1300, with Pantheon $1500, Houses cost $200 each, Hotels: $200 each plus 4 houses 
+Italia: Rent $50, With 1 Villa $200, With 2 Villas $600, With 3 Villas $1400 With 4 Villas $1700, with Pantheon $2000, Houses cost $200 each, Hotels: $200 each plus 4 houses
+
+Utilities rent 4 times dice roll, 2 owned: 10 times dice roll
+Roads or Via: $25 rent, 2 roads: $50, 3 roads: $100, 4 roads: $200
+*/
