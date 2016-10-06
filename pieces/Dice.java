11562ef083ccd88d4a6a1b8b27d85a92642cc322
Brittany <bfram2@uis.edu>
2016-10-06 01:00:19 -0500

    Update Dice.java

--- pieces/Dice.java
+++ pieces/Dice.java
@@ -1,37 +1,70 @@
 // 2 6-sided dice rolls
-//random pieces of code to be gutted and made proper later
+//outputs text only, no images right now
+package pieces;
 import java.awt.Container;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
-// 2 random numbers between 1-6 to simulate dice
-public void roll() {
-  int d6result = 0;
-  int d6result2 = 0;
-  JLabel d6one = new JLabel(getter.getIcon("dice1.jpeg")); //create images later
-  JLabel d6two = new JLabel(getter.getIcon("dice1.jpeg"));
-  JButton button = new JButton("Roll");
-  JLabel text = new JLabel("Two");
-  JFrame window = new JFrame("Roll the dice");
-  Container cp = window.getContentPane();
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
+import javax.swing.ImageIcon;
   
-  d6result = (int)(Math.random() * 6) + 1;
-  d6result2 = (int)(Math.random() * 6) + 1;
-  int d6move = d6result + d6result2;
-  //debugging: System.out.println("Roll: total= " + d6move)
-  //insert call to dice images that match the number
-}
-//return dice total to move total spaces
-public int getValue() {
-  
-  return d6move;
-}
+class Dice {
+	//JButton roll;
+	static int die1;   // Number showing on the first die.
+    static int die2;   // Number showing on the second die.
+    static int diet;   // Number showing on the total die.
+	
+        public Dice() {
+        	//JLabel dice1 = new JLabel(new ImageIcon("src/DiceImages/Dice1.png"));
+        	//JLabel dice2 = new JLabel(new ImageIcon("src/DiceImages/Dice2.png"));
+        	//add(dice1);
+        	//add(dice2);
+        	
+        	//roll = new JButton("Roll");
+        	//JFrame window = new JFrame("Roll the dice");
 
-//check for doubles, if true it is a double
-public boolean Doubles() {
- return d6result == d6result2 & d6result + d6result2 != 0;
- //check for true, then activate movement using d6move
- //player rolls again or gets out of jail
-}
+        
+                // Constructor.  Rolls the dice, so that they initially
+                // show some random values.
+            roll();  // Call the roll() method to roll the dice.
+        }
+        
+        public void roll() {
+                // Roll the dice by setting each of the dice to be
+                // a random number between 1 and 6.
+            die1 = (int)(Math.random()*6) + 1;
+            die2 = (int)(Math.random()*6) + 1;
+            diet = die1 + die2;
+           
+        }
+                 
+        public int getDie() {
+              // Return the number showing on the first die.
+           return die1 + die2;
+           
+        }
+        
+        public int getTotal() {
+              // Return the total showing on the two dice.
+           return diet;
+        }
+
+     
+     public static void main(String args[]) {
+ 		new Dice(); //open menu page
+ 		System.out.println("Dice 1: "+die1);
+ 		System.out.println("Dice 2: "+die2);
+ 		System.out.println("Dice Total: "+diet);
+ 		if (die1 == die2) {
+ 			System.out.println("Doubles! Roll again.");
+ 			//turn active listener to true and wait for button to roll again
+        }
+ 		else {
+ 			System.out.println("Move to next phase or end turn.");
+ 			//buy, pay, draw, end
+ 			}
+ 		}
+     } 
