68fee4454f7d449e4403c2f66e47dbf9cfa5fead
Brittany <bfram2@uis.edu>
2016-10-06 23:20:56 -0500

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -1,5 +1,4 @@
 //The Main Menu for the game to start and to pick the players, type of game, citation, and help.
-//This is the main file
 /* Trademark of Hasboro and Parker Brothers */
 
 //guide to implement folders into master
@@ -8,16 +7,15 @@
 //import events.*;
 //import cards.*;
 
-import pieces.Board; //activate board
+import menu.HelpMenu;
+import pieces.Board;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
-
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
@@ -26,13 +24,13 @@ import java.awt.GridLayout;
 class MainMenu extends JFrame implements ActionListener {
 JButton game, set, help, bear;
 
+
 	public MainMenu() {
 	setTitle("Monopoly");
 	setSize(743,743); //window size
 	setLocationRelativeTo(null);
 	setDefaultCloseOperation(EXIT_ON_CLOSE);
 	setVisible(true);
-	//debugging size by content
 	//pack();
 	setLayout(new BorderLayout());
 	JLabel background=new JLabel(new ImageIcon("src/menu/menuman.jpg"));
@@ -41,7 +39,7 @@ JButton game, set, help, bear;
 	//background.setOpaque(true);
 	
 	//style and location of buttons
-	game=new JButton("<html><center><div style=\"color: white; font-weight: bold; font-family: verdana; font-size: 14pt; padding: 5px;\"><u>S</u>tart New Game</div>");
+	game=new JButton("<html><center><div style=\"color: white; font-weight: bold; font-family: verdana; font-size: 14pt; padding: 5px 50px 5px 50px;\"><u>S</u>tart New Game</div>");
 	set=new JButton("<html><center><div style=\"color: white; font-weight: bold; font-family: verdana; font-size: 14pt; padding: 5px;\"><u>S</u>ettings</div>");
 	help=new JButton("<html><center><div style=\"color: white; font-weight: bold; font-family: verdana; font-size: 14pt; padding: 5px;\"><u>H</u>elp</div>");
 	bear=new JButton("<html><center><div style=\"color: white; font-weight: bold; font-family: verdana; font-size: 14pt; padding: 5px;\"><u>E</u>xit</div>");
@@ -57,18 +55,14 @@ JButton game, set, help, bear;
 	help.addActionListener(this);
 	bear.addActionListener(this);
 	
-	//add button to jpanel
+	//add buttons
 	background.add(game);
 	background.add(set);
 	background.add(help);
 	background.add(bear);
 
 	// refresh, DO NOT remove
-	//setSize(744,744); redundancy
 	setSize(744,744);
-	
-	//debugging button locations
-	//System.out.println("getLocation: " + game.getLocation());
 	}
 	
 	public void actionPerformed(ActionEvent e)
@@ -85,7 +79,8 @@ JButton game, set, help, bear;
 		}
 		else if(e.getSource() == help) {
 			//settings for user guide
-			System.out.println("Monopoly is Awesome, help!");
+			new HelpMenu();
+			//System.out.println("Monopoly is Awesome, help!");
 		}
 		else if(e.getSource() == bear) {
 			//close the program.. exit bear, stage left
