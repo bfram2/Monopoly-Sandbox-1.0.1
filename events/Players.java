2ae14d8b56f37d080566a9c86466a5c91830acb4
Brittany <bfram2@uis.edu>
2016-10-07 08:39:49 -0500

    Update Players.java

--- events/Players.java
+++ events/Players.java
@@ -1,3 +1,10 @@
 //Switching between Player and Computer Players in the amount choosen in the main menu
 //each player starts with 1500 denarius
+//Player 1 is first
+//Start at Rome
+//Roll, move that number of spaces
+//buy, pay rent, draw card, pay taxes or go to the arena
+//buy villas or pantheon
+//end turn button
+//Player 2, repeat onward
 /* Later may have multiple players with computer players */
