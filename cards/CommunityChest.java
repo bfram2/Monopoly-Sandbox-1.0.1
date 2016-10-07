26958255c1addfe583ddc7ec91cae4ae188d859e
Brittany <bfram2@uis.edu>
2016-10-07 13:04:10 -0500

    Update CommunityChest.java

--- cards/CommunityChest.java
+++ cards/CommunityChest.java
@@ -1,20 +1,21 @@
 //For Community Chest Cards, activate using images or background image with text.
 
 /*
-Bank error in your favor. Collect $200.
-Pay private physician fees of $100.
-From sale of merchant carts, you get $50.
-You inherit a small Villa, gain $100.
-Go to the Gladiatorial Arena. Go directly to the arena, DO NOT pass Rome, DO NOT collect $200.
-Advance to Rome. Collect $200.
-Render too much unto Caesar, Collect $200.
-Roman official assesses you for street repairs: $40 per villa, $115 per hotel.
-You have won the second price in the Campus Track & Field foot race. Collect $10.
-Your properties and assets mature, join the Equestrian Order. Collect $100. [19]
-Sell bulk staples to the bank, receive $100.
-Pay farmer’s tax of $25.
-You have a coming of age party, Collect $10 from every player.
-Pay school fees of $50.
-Medical institution fees, pay $50.
-Gain a favor with a Senator, get out of the Gladiatorial Area for free. This card may be kept until needed or traded.
+src/cards/images/Chest1-16.PNG
+1. Bank error in your favor. Collect $200.
+2. Pay private physician fees of $100.
+3. From sale of merchant carts, you get $50.
+4. You inherit a small Villa, gain $100.
+5. Go to the Gladiatorial Arena. Go directly to the arena, DO NOT pass Rome, DO NOT collect $200.
+6. Advance to Rome. Collect $200.
+7. Render too much unto Caesar, Collect $200.
+8. Roman official assesses you for street repairs: $40 per villa, $115 per pantheon.
+9. You have won the second price in the Campus Track & Field foot race. Collect $10.
+10. Your properties and assets mature, join the Equestrian Order. Collect $100.
+11. Sell bulk staples to the bank, receive $100.
+12. Pay farmer’s tax of $25.
+13. You have a coming of age party, Collect $10 from every player.
+14. Pay school fees of $50.
+15. Medical institution fees, pay $50.
+16. Gain a favor with a Senator, get out of the Gladiatorial Arena for free. This card may be kept until needed or traded.
 */
