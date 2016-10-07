5b5e57d7a574809909b8bd5b13bbf6b4ab9a9084
Brittany <bfram2@uis.edu>
2016-10-07 08:27:33 -0500

    Update Players.java

--- events/Players.java
+++ events/Players.java
@@ -1,2 +1,3 @@
 //Switching between Player and Computer Players in the amount choosen in the main menu
+//each player starts with 1500 denarius
 /* Later may have multiple players with computer players */
