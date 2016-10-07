8a2ed9d5631c1ed31afcd7f99ecb3f1412d64cbe
Brittany <bfram2@uis.edu>
2016-10-07 11:53:23 -0500

    Update UtilitiesRoads.java

--- cards/UtilitiesRoads.java
+++ cards/UtilitiesRoads.java
@@ -1,12 +1,17 @@
 //For implementing the Utilities and Road cards, use image or background image over text.
 
 /*
-Aqueducts: (If one “Utility” is owned, rent is 4 times amount shown on dice. If both “Utilities” are owned, rent is 10 times amount shown on dice. Mortgage Value $75)
-City Sewage: (If one “Utility” is owned, rent is 4 times amount shown on dice. If both “Utilities” are owned, rent is 10 times amount shown on dice. Mortgage Value $75)
-Roads or Via:
+1 utility: 4 times dice total. both utilities: are 10 times dice total
+   Aqueducts
+   Sewers
+
+Rent: 25
+2 owned: 25*2
+3 owned: 50*2
+4 owned: 100*2
    Via Appia
    Via Flaminia
    Via Aemilia
    Via Popillia
-Road Text: RENT $25, If 2 Via are owned $50, If 3 Via are owned $100, If 4 Via are owned $200, Mortage Value $100
+
 */
