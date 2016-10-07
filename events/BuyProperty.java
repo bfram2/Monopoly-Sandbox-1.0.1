0ea7a70174fdd2dcdd94861c702dd8e8a5903f95
Brittany <bfram2@uis.edu>
2016-10-07 08:48:20 -0500

    Update BuyProperty.java

--- events/BuyProperty.java
+++ events/BuyProperty.java
@@ -1,38 +1,15 @@
 //Flat out buying any property & Mortaging it for half of the cost of buying the property
 
 /*
-Purple (#44235A)
-•	Mediterranean Avenue = Germania Inferior ($60, $30 mortgaged)
-•	Baltic Avenue = Germania Superior ($60, $30 mortgaged)
-Baby Blue (#C4C8E5)
-•	Oriental Avenue = Alpes Poeniae ($100, $50 mortgaged)
-•	Vermont Avenue = Alpes Cottiae ($100, $50 mortgaged)
-•	Connecticut Avenue = Aples Maritimae ($120, $60 mortgaged)
-Dark Pink (#A72758)
-•	St. Charles Place = Aquitania ($140, $70 mortgaged)
-•	States Avenue = Belgica ($140, $70 mortgaged)
-•	Virginia Avenue = Raetia ($160, $80 mortgaged)
-Orange (#F19749)
-•	St. James Place = Africa Proconsularis ($180, $90 mortgaged)
-•	Tennessee Avenue = Asia ($180, $90 mortgaged)
-•	New York Avenue = Britannia ($200, $100 mortgaged)
-Red (#E22835)
-•	Kentucky Avenue = Cilicia ($220, $110 mortgaged)
-•	Indiana Avenue = Galatia ($220, $110 mortgaged)
-•	Illinois Avenue = Cappadocia ($240, $120 mortgaged)
-Yellow (#F8F37B)
-•	Atlantic Avenue = Aegyptus ($260, $130 mortgaged)
-•	Ventnor Avenue = Arabia Petraea ($260, $130 mortgaged)
-•	Marvin Gardens = Syria ($280, $140 mortgaged)
-Green (#448364)
-•	Pacific Avenue = Macedonia ($300, $150 mortgaged)
-•	North Carolina Avenue = Epirus ($300, $150 mortgaged)
-•	Pennsylvania Avenue = Achaia ($320, $160 mortgaged)
-Blue (#413382)
-•	Park Place = Sicilia ($350, $175 mortgaged)
-•	Boardwalk = Italia ($400, $200 mortgaged)
-Utilities
-$150 buy, $75 mortgaged
-Roads
-$200 buy, $100 mortgaged
+Buy (Mortgage is buy/2 with no rent on item until other half is paid to the bank)
+Purple: Germania Inferior $60, Germania Superior $60
+Baby Blue: Alpes Poeniae $100, Alpes Cottiae $100, Aples Maritimae $120
+Dark Pink: Aquitania $140, Belgica $140, Raetia $160
+Orange: Africa Proconsularis $180, Asia $180, Britannia $200
+Red: Cilicia $220, Galatia $220, Cappadocia $240
+Yellow: Aegyptus $260, Arabia Petraea $260, Syria $280
+Green: Macedonia $300, Epirus $300, Achaia $320
+Blue: Sicilia $350, Italia $400
+Via: $200
+Water: $150
 */
