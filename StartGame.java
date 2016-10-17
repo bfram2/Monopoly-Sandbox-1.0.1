d854db373f64516c460afaac91c43a883a65456c
Brittany <bfram2@uis.edu>
2016-10-16 21:07:13 -0500

    Update and rename MainMenu.java to StartGame.java

---
+++ StartGame.java
@@ -0,0 +1,18 @@
+//The Main Menu for the game to start and to pick the players, type of game, citation, and help.
+/* Trademark of Hasboro and Parker Brothers */
+
+//guide to implement folders into master
+//import pieces.*;
+//import menu.*;
+//import events.*;
+//import cards.*;
+//start the game
+import menu.MainMenu;
+
+public class StartGame {
+	//main
+	public static void main(String args[]) {
+	new MainMenu(); //open menu page
+	
+	}
+}
