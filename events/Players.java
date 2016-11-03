b92b23e74990945902c1e1833c1cd82994de52c4
Brittany <bfram2@uis.edu>
2016-11-03 15:45:48 -0500

    Update Players.java

--- events/Players.java
+++ events/Players.java
@@ -3,12 +3,13 @@ package events;
 //Use images of the dollar bills or money counter with just the digits ie. 100.00 denarius
 //int k = 0; //doubles counter
 //int j = 0; //jail counter
-//int g = 0; //pass go counter
+//int oj = 0; //out of jail counter
 public class Players {
 	private String name;
 	private String token;
 	private int position;
 	private int balance;
+	private int k, j, oj;
 	String player;
 
 	public Players() {
@@ -16,16 +17,30 @@ public class Players {
         token = "Token_W1.png"; //default image, update from menu
         position = 0;          //update from Prev in Board
         balance = 1500;        //update from Bal in Board
+        k = 0;                 //counters
+	j = 0;
+	oj = 0;	
         player = (name+token+position+balance);
 	}
 	
-	public Players(String name, String token, int position, int balance) {
+	public Players(String name, String token, int position, int k, int j, int oj, int balance) {
 	        this.name = name;
 	        this.token = token;
 	        this.position = position;
 	        this.balance = balance;
+		this.k = k;
+		this.j = j;
+		this.oj = oj;
 	}
+	public int getDoubles() {return k;}
+	public void setDoubles(int k) {this.k = k;}
 	
+	public int getJail() {return j;}
+	public void setJail() {this.j = j;}
+	
+	public int getOutJail() {return oj;}
+	public void setOutJail() {this.oj = oj;}
+		
 	public String getName() {return name;}
 	public void setName(String name) {this.name = name;}
 	
