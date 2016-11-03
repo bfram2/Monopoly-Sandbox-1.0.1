8049cbfabbe3aa0ddfff7fa20dace46407a4f928
Brittany <bfram2@uis.edu>
2016-11-03 17:09:11 -0500

    Update Chance.java

--- cards/Chance.java
+++ cards/Chance.java
@@ -1,34 +1,15 @@
 package cards;
-import pieces.*;
+import java.util.Random;
+//import pieces.*;
 import events.Players;
-//For Image Chance Cards and their events
+//For image Chance Cards and their events
 
-/*
-1.Take a trip to Via Appia. If you pass Rome collect $200.
-2,3. Advance to the nearest Via. If UNOWNED, you may buy it from the Bank. 
-If OWNED, pay owner twice the rental to which they are otherwise entitled.
 
-4. Gain a favor with a Senator, get out of the Gladiatorial Arena for free. This card may be kept until needed or traded.
-5. Advance to the nearest Utility. If UNOWNED, you may buy it from the Bank. 
-If OWNED, throw dice and pay owner a total ten times amount thrown.
-
-6. Advance to Rome. Collect $200.
-7. A new road is built near your Villa raising its value, collect $150.
-8. Go back 3 spaces.
-9. Make general repairs on all of your properties: For villa pay $25, For pantheons pay $100.
-10. Advance to Italia.
-11. Recent investment in more merchant carts gains you $50.
-12. Your horses escape into the city. Pay a $15 fine.
-13. Go to the Gladiatorial Arena. Go directly to the arena, DO NOT pass Rome, DO NOT collect $200.
-14. Advance to Aquitania. If you pass Rome, Collect $200.
-15. Advance to Cappadocia. If you pass Rome, Collect $200.
-16. You have been elected a Senator, pay each player $50.
-*/
 	public class Chance {
 		Players thePlayer = new Players();     //need position, balance, oj, k & j counters
 		int PrevPos = thePlayer.getPosition(); //player's current position
 		int PrevBal = thePlayer.getBalance();  //player's current balance
-		int outjail = thePlayer.getOutJail();  //get out of jail counter
+		int Outjail = thePlayer.getOutJail();  //get out of jail counter
 		int jail = thePlayer.getJail();        //in jail, cannot pass go if(j=0)
 
 		private int chanceno;
@@ -66,7 +47,7 @@ If OWNED, throw dice and pay owner a total ten times amount thrown.
 				}
 			} //nearest Via, if owned: pay 2(rent), if unowned can buy
 			if (chanceno == 4) {
-				outjail++; //get out of jail for free
+				Outjail++; //get out of jail for free
 			}
 			if (chanceno == 5) {
 				if (PrevPos > 28 || PrevPos < 13) {
@@ -118,11 +99,31 @@ If OWNED, throw dice and pay owner a total ten times amount thrown.
 
 			thePlayer.setPosition(PrevPos); //set Player position, counters & balance
 			thePlayer.setBalance(PrevBal);
-			thePlayer.setOutJail(outjail);
+			thePlayer.setOutJail(Outjail);
 		 } //can't be in jail
 		}  //end pull
 		public int getChanceNo() {return chanceno;}
-		public int getImgName() {return imgname;} 
+		public String getImgName() {return imgname;} 
 		public int getAdjust() {return adjust;}
 }
+	/*
+	1.Take a trip to Via Appia. If you pass Rome collect $200.
+	2,3. Advance to the nearest Via. If UNOWNED, you may buy it from the Bank. 
+	If OWNED, pay owner twice the rental to which they are otherwise entitled.
 
+	4. Gain a favor with a Senator, get out of the Gladiatorial Arena for free. This card may be kept until needed or traded.
+	5. Advance to the nearest Utility. If UNOWNED, you may buy it from the Bank. 
+	If OWNED, throw dice and pay owner a total ten times amount thrown.
+
+	6. Advance to Rome. Collect $200.
+	7. A new road is built near your Villa raising its value, collect $150.
+	8. Go back 3 spaces.
+	9. Make general repairs on all of your properties: For villa pay $25, For pantheons pay $100.
+	10. Advance to Italia.
+	11. Recent investment in more merchant carts gains you $50.
+	12. Your horses escape into the city. Pay a $15 fine.
+	13. Go to the Gladiatorial Arena. Go directly to the arena, DO NOT pass Rome, DO NOT collect $200.
+	14. Advance to Aquitania. If you pass Rome, Collect $200.
+	15. Advance to Cappadocia. If you pass Rome, Collect $200.
+	16. You have been elected a Senator, pay each player $50.
+	*/
